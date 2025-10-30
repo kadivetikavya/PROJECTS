5953b35f49490d0fa65e448d6df199f93c92311f
kavya kadiveti <156996322+kadivetikavya@users.noreply.github.com>
2025-10-30 12:42:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-echo "console.log('Hello from Kavya's secure pipeline!');"
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return "Welcome to Batch11!"
+
+@app.route('/how are you')
+def hello():
+    return 'I am good, how about you?'
+
+if __name__ == "__main__":
+    app.run()
